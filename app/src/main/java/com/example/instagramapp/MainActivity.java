e634b767921ad51d91ea0f50662f658c19f3b2bc
Maria Burguete Euan <majoburguete@fb.com>
2021-07-05 14:54:39 -0500

    Setting the view binding and the support action bar

--- app/src/main/java/com/example/instagramapp/MainActivity.java
+++ app/src/main/java/com/example/instagramapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.instagramapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+
+import com.example.instagramapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setContentView(binding.getRoot());
     }
 }
