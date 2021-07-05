bf13cbe20af3424542352ea42d6be0da08376bb1
Maria Burguete Euan <majoburguete@fb.com>
2021-07-05 15:51:41 -0500

    Applied view binding and setted the support action bar

--- app/src/main/java/com/example/instagramapp/PhotoActivity.java
+++ app/src/main/java/com/example/instagramapp/PhotoActivity.java
@@ -3,12 +3,18 @@ package com.example.instagramapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+
+import com.example.instagramapp.databinding.ActivityPhotoBinding;
 
 public class PhotoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_photo);
+        ActivityPhotoBinding binding = ActivityPhotoBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        Toolbar toolbar = findViewById(R.id.photoToolbar);
+        setSupportActionBar(toolbar);
     }
 }
