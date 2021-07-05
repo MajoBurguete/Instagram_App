a3bf9ee64e9898181853c033750b01553f65de01
Maria Burguete Euan <majoburguete@fb.com>
2021-07-05 16:57:25 -0500

    Registered the parse object

--- app/src/main/java/com/example/instagramapp/ParseApplication.java
+++ app/src/main/java/com/example/instagramapp/ParseApplication.java
@@ -3,12 +3,16 @@ package com.example.instagramapp;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
     // Initialize Parse SDK as soon as the application is created
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
 
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("NgIxfZhJF3ajHzKTKOVmlK1flDNEOhEK685uLjMI")
