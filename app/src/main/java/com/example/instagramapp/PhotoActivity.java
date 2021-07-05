459ffedf5de311549ae40bc09d3d4e2888c1276b
Maria Burguete Euan <majoburguete@fb.com>
2021-07-05 16:57:53 -0500

    We can now get all of the posts

--- app/src/main/java/com/example/instagramapp/PhotoActivity.java
+++ app/src/main/java/com/example/instagramapp/PhotoActivity.java
@@ -4,14 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import androidx.appcompat.widget.Toolbar;
 
 import com.example.instagramapp.databinding.ActivityPhotoBinding;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 public class PhotoActivity extends AppCompatActivity {
+
+    private String TAG = "PhotoActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +30,26 @@ public class PhotoActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.photoToolbar);
         setSupportActionBar(toolbar);
 
+        queryPosts();
 
+    }
 
+    //Getting all of the posts
+    private void queryPosts() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if(e != null){
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                }
+                for (Post post : posts){
+                    Log.i(TAG, "Post: " + post.getDescription() + ", Username: " + post.getUser().getUsername());
+                }
+            }
+        });
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
