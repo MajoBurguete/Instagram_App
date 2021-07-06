8ceedd06de0037cabc0d6c6cb6d8cdcff9880230
Maria Burguete Euan <majoburguete@fb.com>
2021-07-06 17:49:05 -0500

    Posts adapter created

--- app/src/main/java/com/example/instagramapp/PostAdapter.java
+++ app/src/main/java/com/example/instagramapp/PostAdapter.java
@@ -1,2 +1,54 @@
-package com.example.instagramapp;public class PostAdapter {
+package com.example.instagramapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
+
+    private Context context;
+    List<Post> rvPosts;
+
+    public PostAdapter(Context context, List<Post> rvPosts) {
+        this.context = context;
+        this.rvPosts = rvPosts;
+    }
+
+    @NonNull
+    @Override
+    public PostAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_post, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PostAdapter.ViewHolder holder, int position) {
+    }
+
+    @Override
+    public int getItemCount() {
+        return rvPosts.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvUsername;
+        TextView tvBody;
+        ImageView ivPict;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvUsername = itemView.findViewById(R.id.tvUsername);
+            tvBody = itemView.findViewById(R.id.tvBody);
+            ivPict = itemView.findViewById(R.id.ivPict);
+        }
+    }
 }
