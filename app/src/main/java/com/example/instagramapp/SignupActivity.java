efe6c88646446a298e975d8a3255b158083faa16
Maria Burguete Euan <majoburguete@fb.com>
2021-07-06 16:15:41 -0500

    Setted the onClick listener for the sign up button

--- app/src/main/java/com/example/instagramapp/SignupActivity.java
+++ app/src/main/java/com/example/instagramapp/SignupActivity.java
@@ -2,14 +2,39 @@ package com.example.instagramapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.instagramapp.databinding.ActivitySignupBinding;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class SignupActivity extends AppCompatActivity {
+
+    private String TAG = "SignupActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
+        ActivitySignupBinding binding = ActivitySignupBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get the values from the text view
+
+                // Create the ParseUser
+
+                // Set properties
+
+                //Invoke signUpInBackground
+            }
+        });
 
 
 
