2add9c3b91e121b5e96634f3f14e014f651e3757
Maria Burguete Euan <majoburguete@fb.com>
2021-07-07 17:20:49 -0500

    Moved all of the logic from photoActivity to the fragment

--- app/src/main/java/com/example/instagramapp/fragments/ComposeFragment.java
+++ app/src/main/java/com/example/instagramapp/fragments/ComposeFragment.java
@@ -1,12 +1,20 @@
 package com.example.instagramapp.fragments;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,16 +24,25 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.instagramapp.PhotoActivity;
+import com.example.instagramapp.Post;
 import com.example.instagramapp.R;
 import com.example.instagramapp.databinding.ActivityPhotoBinding;
+import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.io.File;
+
+import static android.app.Activity.RESULT_OK;
+import static com.example.instagramapp.PhotoActivity.CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE;
 
 public class ComposeFragment extends Fragment {
 
-    private EditText etDescription;
-    private Button btnTake;
-    private Button btnSubmit;
-    private ImageView ivPicture;
+    private String TAG = "PhotoFragment";
+    ImageView ivPicture;
+    private File photoFile;
+    private String photoFileName = "photo.jpg";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -38,13 +55,7 @@ public class ComposeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         ActivityPhotoBinding binding = ActivityPhotoBinding.inflate(getLayoutInflater());
-        Toolbar toolbar = findViewById(R.id.photoToolbar);
-        setSupportActionBar(toolbar);
-        etDescription = binding.etDescription;
-        btnTake = binding.btnTake;
-        btnSubmit = binding.btnSubmit;
         ivPicture = binding.ivPicture;
-
         binding.btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -67,5 +78,79 @@ public class ComposeFragment extends Fragment {
             }
         });
 
+    }
+
+    private void savePost(String description, ParseUser currentUser, File photoFile, ActivityPhotoBinding binding) {
+        Post post = new Post();
+        post.setDescription(description);
+        post.setImage(new ParseFile(photoFile));
+        post.setUser(currentUser);
+        post.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null){
+                    Log.e(TAG, "Error while saving post", e);
+                    Toast.makeText(getContext(), "Error while saving!", Toast.LENGTH_SHORT).show();
+                }
+                Toast.makeText(getContext(), "Post was save successfully", Toast.LENGTH_SHORT).show();
+                binding.etDescription.setText("");
+                binding.ivPicture.setImageResource(0);
+                getActivity().setResult(RESULT_OK);
+                getActivity().finish();
+            }
+        });
+    }
+
+    private void launchCamera() {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference for future access
+        photoFile = getPhotoFileUri(photoFileName);
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(getContext(), "com.codepath.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getContext().getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                // RESIZE BITMAP, see section below
+                // Load the taken image into a preview
+                ivPicture.setImageBitmap(takenImage);
+            } else { // Result was a failure
+                Toast.makeText(getContext(), "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private File getPhotoFileUri(String fileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(TAG, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+
+        return file;
     }
 }
