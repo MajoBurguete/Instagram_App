009ee14bea1a0a04a37576258b50edcd35586e83
Maria Burguete Euan <majoburguete@fb.com>
2021-07-07 20:26:20 -0500

    Added method to get the data from the database

--- app/src/main/java/com/example/instagramapp/fragments/HomeFragment.java
+++ app/src/main/java/com/example/instagramapp/fragments/HomeFragment.java
@@ -9,6 +9,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,11 +18,15 @@ import com.example.instagramapp.Post;
 import com.example.instagramapp.PostAdapter;
 import com.example.instagramapp.R;
 import com.example.instagramapp.databinding.FragmentHomeBinding;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 
 import java.util.List;
 
 public class HomeFragment extends Fragment {
 
+    private String TAG = "HomeFragment";
     RecyclerView rvPosts;
 
     public HomeFragment(){
@@ -41,5 +46,31 @@ public class HomeFragment extends Fragment {
 
         // Defining the recycler view
         rvPosts = binding.rvPosts;
+
+        // query posts from Parstagram
+        queryPosts();
     }
+
+    //Getting all of the posts
+    private void queryPosts() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.setLimit(20);
+        query.addDescendingOrder("createdAt");
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if(e != null){
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                }
+                //postAdapter.clear();
+                for (Post post : posts){
+                    Log.i(TAG, "Post: " + post.getDescription() + ", Username: " + post.getUser().getUsername());
+                }
+                //postAdapter.addAll(posts);
+            }
+        });
+    }
+
 }
