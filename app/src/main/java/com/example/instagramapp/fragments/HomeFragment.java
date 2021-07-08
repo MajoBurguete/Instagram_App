143655ca3f9f45b974b7eac4450594c19ee0a6bb
Maria Burguete Euan <majoburguete@fb.com>
2021-07-07 20:21:58 -0500

    Got a reference to the recycler view in the home fragment layout

--- app/src/main/java/com/example/instagramapp/fragments/HomeFragment.java
+++ app/src/main/java/com/example/instagramapp/fragments/HomeFragment.java
@@ -2,15 +2,27 @@ package com.example.instagramapp.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.instagramapp.Post;
+import com.example.instagramapp.PostAdapter;
 import com.example.instagramapp.R;
+import com.example.instagramapp.databinding.FragmentHomeBinding;
+
+import java.util.List;
 
 public class HomeFragment extends Fragment {
+
+    RecyclerView rvPosts;
 
     public HomeFragment(){
     }
@@ -20,5 +32,14 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        FragmentHomeBinding binding = FragmentHomeBinding.inflate(getLayoutInflater());
+
+        // Defining the recycler view
+        rvPosts = binding.rvPosts;
     }
 }
