4747cd47abfd55489903dbc010a8c5fab2d38fc8
Maria Burguete Euan <majoburguete@fb.com>
2021-07-09 18:09:33 -0500

    Post adapter was created and initialized

--- app/src/main/java/com/example/instagramapp/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/instagramapp/fragments/ProfileFragment.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,19 +16,26 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.instagramapp.Post;
+import com.example.instagramapp.PostAdapter;
 import com.example.instagramapp.R;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ProfileFragment extends Fragment {
+public class ProfileFragment extends Fragment implements PostAdapter.OnClickListener{
 
+    private String TAG = "ProfileFragment";
     ImageView ivProfPict;
     TextView tvProfileName;
     TextView tvNameP;
     TextView tvProfileDesc;
     RecyclerView rvPosts;
     List<Post> postsUser;
+    PostAdapter adapter;
     private SwipeRefreshLayout swipeContainer;
 
     public ProfileFragment() {
@@ -53,6 +61,9 @@ public class ProfileFragment extends Fragment {
         // Get the reference from the recycler view in the profile layout
         rvPosts = view.findViewById(R.id.rvUserPosts);
 
+        // Defining the adapter
+        adapter = new PostAdapter(getContext(), postsUser, this);
+
         // Initialize post array
         postsUser = new ArrayList<>();
 
@@ -62,6 +73,21 @@ public class ProfileFragment extends Fragment {
             public void onRefresh() {
             }
         });
+
+    }
+
+    @Override
+    public void onLikeClick(int position) {
+
+    }
+
+    @Override
+    public void onCommentsClick(int position) {
+
+    }
+
+    @Override
+    public void onCommentButtonClick(int position) {
 
     }
 }
