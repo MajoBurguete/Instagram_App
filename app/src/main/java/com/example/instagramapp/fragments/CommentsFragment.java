8e5382253ab1dc30c8baf5d7f109080f676783a6
Maria Burguete Euan <majoburguete@fb.com>
2021-07-09 00:08:11 -0500

    Method that allows me to get all of the comments

--- app/src/main/java/com/example/instagramapp/fragments/CommentsFragment.java
+++ app/src/main/java/com/example/instagramapp/fragments/CommentsFragment.java
@@ -1,17 +1,31 @@
 package com.example.instagramapp.fragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.instagramapp.Comment;
+import com.example.instagramapp.Post;
 import com.example.instagramapp.R;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
 
 public class CommentsFragment extends Fragment {
+
+    private String TAG = "CommentsFragment";
+
     public CommentsFragment() {
         // Required empty public constructor
     }
@@ -26,5 +40,27 @@ public class CommentsFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        queryComments();
+    }
+
+    //Getting all of the posts
+    private void queryComments() {
+        ParseQuery<Comment> query = ParseQuery.getQuery(Comment.class);
+        query.include(Comment.KEY_USER);
+        query.include(Comment.KEY_POST);
+        query.setLimit(20);
+        query.addDescendingOrder("createdAt");
+        query.findInBackground(new FindCallback<Comment>() {
+            @Override
+            public void done(List<Comment> comments, ParseException e) {
+                if(e != null){
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                }
+                for (Comment comment: comments){
+                    Log.i(TAG, "Comment: " + comment.getComment() + ", Username: " + comment.getUser().getUsername() + ", post: "+comment.getPost().getString("objectId"));
+                }
+            }
+        });
     }
 }
