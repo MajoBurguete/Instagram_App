19f3b6a7372655a84b4f57aa9e197d959de18f5e
Maria Burguete Euan <majoburguete@fb.com>
2021-07-09 00:20:35 -0500

    Adapter constructor was created

--- app/src/main/java/com/example/instagramapp/CommentAdapter.java
+++ app/src/main/java/com/example/instagramapp/CommentAdapter.java
@@ -1,12 +1,23 @@
 package com.example.instagramapp;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.ViewHolder> {
+
+    private Context context;
+    List<Comment> rvComments;
+
+    public CommentAdapter(Context context, List<Comment> commentList) {
+        this.context = context;
+        this.rvComments = commentList;
+    }
 
     @NonNull
     @Override
