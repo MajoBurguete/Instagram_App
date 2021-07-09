31d85c5fce3c8d8bd97f196606dfc09b0efd383c
Maria Burguete Euan <majoburguete@fb.com>
2021-07-09 18:12:46 -0500

    The method to do the query and get all of the posts that the current user has done has been implemented

--- app/src/main/java/com/example/instagramapp/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/instagramapp/fragments/ProfileFragment.java
@@ -76,9 +76,32 @@ public class ProfileFragment extends Fragment implements PostAdapter.OnClickList
         swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
+                queryPosts();
+                swipeContainer.setRefreshing(false);
             }
         });
 
+    }
+
+    //Getting all of the posts
+    private void queryPosts() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.whereEqualTo(Post.KEY_USER, ParseUser.getCurrentUser());
+        query.setLimit(20);
+        query.addDescendingOrder("createdAt");
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if(e != null){
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                }
+                for (Post post : posts){
+                    Log.i(TAG, "Post: " + post.getDescription() + ", Username: " + post.getUser().getUsername());
+                }
+            }
+        });
     }
 
     @Override
