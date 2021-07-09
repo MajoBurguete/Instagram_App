fb4cbe4ebaa878b68eea47256b08108e5d6a8029
Maria Burguete Euan <majoburguete@fb.com>
2021-07-09 17:52:04 -0500

    Got all of the references from the profile layout and initialized the post array

--- app/src/main/java/com/example/instagramapp/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/instagramapp/fragments/ProfileFragment.java
@@ -5,14 +5,29 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.instagramapp.Post;
 import com.example.instagramapp.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProfileFragment extends Fragment {
+
+    ImageView ivProfPict;
+    TextView tvProfileName;
+    TextView tvNameP;
+    TextView tvProfileDesc;
+    RecyclerView rvPosts;
+    List<Post> postsUser;
+
     public ProfileFragment() {
         // Required empty public constructor
     }
@@ -27,5 +42,15 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        ivProfPict = view.findViewById(R.id.ivProfPict);
+        tvNameP = view.findViewById(R.id.tvNameP);
+        tvProfileName = view.findViewById(R.id.tvProfileName);
+        tvProfileDesc = view.findViewById(R.id.tvProfileDesc);
+
+        // Get the reference from the recycler view in the profile layout
+        rvPosts = view.findViewById(R.id.rvUserPosts);
+
+        // Initialize post array
+        postsUser = new ArrayList<>();
     }
 }
