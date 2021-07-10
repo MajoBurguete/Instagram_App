d7b07a8c5f77470cac5412f2b02d0eaa6818beba
Maria Burguete Euan <majoburguete@fb.com>
2021-07-09 20:07:44 -0500

    saveDescription method was implemented to edit the profile description of the current user

--- app/src/main/java/com/example/instagramapp/EditActivity.java
+++ app/src/main/java/com/example/instagramapp/EditActivity.java
@@ -49,6 +49,22 @@ public class EditActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String newDesc = etDescEdit.getText().toString();
+                saveDescription(newDesc, user);
+            }
+        });
+
+    }
+
+    private void saveDescription(String description, ParseUser currentUser) {
+        currentUser.put(KEY_PROFILEDESC, description);
+        currentUser.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null){
+                    Log.e(TAG, "Error while saving description", e);
+                    Toast.makeText(EditActivity.this, "Error while saving!", Toast.LENGTH_SHORT).show();
+                }
+                Toast.makeText(EditActivity.this, "Your description was save successfully", Toast.LENGTH_SHORT).show();
             }
         });
 
